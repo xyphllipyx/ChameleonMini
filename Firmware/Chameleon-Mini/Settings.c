ff461637ff27edc0122d6489da0d8e9c82d10da5
Willok <Willok@163.com>
2019-12-24 12:22:11 +0800

    modify default setting, add default card data

--- Firmware/Chameleon-Mini/Settings.c
+++ Firmware/Chameleon-Mini/Settings.c
@@ -125,13 +125,12 @@ bool SettingsSetActiveById(uint8_t Setting) {
             GlobalSettings.ActiveSettingPtr =
                 &GlobalSettings.Settings[SettingIdx];
 
-            /* Recall new memory contents ( Moved this to allow for Access to new Memory in Application init())*/
-            MemoryRecall();
-
             /* Settings have changed. Progress changes through system */
             ConfigurationSetById(GlobalSettings.ActiveSettingPtr->Configuration);
             LogSetModeById(GlobalSettings.ActiveSettingPtr->LogMode);
 
+            /* Recall new memory contents ( ApplicationInitFunc() will be called again )*/
+            MemoryRecall();
 
             SETTING_UPDATE(GlobalSettings.ActiveSettingIdx);
             SETTING_UPDATE(GlobalSettings.ActiveSettingPtr);
